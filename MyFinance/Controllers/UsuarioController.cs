934bee1033157094bce9964521319360d54cf468
Hebert Talles <talles_be@hotmail.com>
2020-01-16 14:08:01 -0300

    Implementada a validacao previa do login de Usuario.

---
+++ MyFinance/Controllers/UsuarioController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyFinance.Models;
+
+namespace MyFinance.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ValidarLogin(UsuarioModel usuario)
+        {
+            if (usuario.ValidarLogin())
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                TempData["MensagemLoginInvalido"] = "Dados de login inválidos!";
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
---
+++ MyFinance/Models/UsuarioModel.cs
@@ -0,0 +1,46 @@
+﻿using MyFinance.Util;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFinance.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public UsuarioModel()
+        {
+        }
+        public UsuarioModel(int id, string nome, string email, string senha)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Senha = senha;
+        }
+
+        public bool ValidarLogin()
+        {
+            string query = $"select Id, Nome, Data_Nascimento from Usuario where Email = '{Email}' and Senha = '{Senha}'";
+            DAL objDAL = new DAL();
+            DataTable dt = objDAL.Reader(query);
+
+            if(dt != null)
+            {
+                if(dt.Rows.Count == 1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
