419f0719977eaf9bf4a77a361ae1b264252ea3f1
Hebert Talles <talles_be@hotmail.com>
2020-01-21 13:47:55 -0300

    Implementado o CRUD de Plano de Contas

--- MyFinance/Controllers/PlanoContaController.cs
+++ MyFinance/Controllers/PlanoContaController.cs
@@ -2,15 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyFinance.Models;
 
 namespace MyFinance.Controllers
 {
     public class PlanoContaController : Controller
     {
+        IHttpContextAccessor HttpContextAccessor;
+
+        public PlanoContaController(IHttpContextAccessor httpContextAccessor)
+        {
+            HttpContextAccessor = httpContextAccessor;
+        }
+
         public IActionResult Index()
         {
+            PlanoContaModel obj = new PlanoContaModel(HttpContextAccessor);
+            ViewBag.ListaPlanoContas = obj.ListaPlanoContas();
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CriarPlanoConta(PlanoContaModel planoConta)
+        {
+            if (ModelState.IsValid)
+            {
+                planoConta.HttpContextAccessor = HttpContextAccessor;
+                if (planoConta.Id != 0)
+                {
+                    planoConta.AlterarPlanoConta();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    planoConta.RegistrarPlanoConta();
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult CriarPlanoConta(int? id)
+        {
+            if (id != null)
+            {
+                PlanoContaModel obj = new PlanoContaModel(HttpContextAccessor);
+                ViewBag.Registro = obj.CarregarRegistro(id);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ExcluirPlanoConta(int id)
+        {
+            new PlanoContaModel().ExcluirPlanoConta(id);
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ MyFinance/Models/PlanoContaModel.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using MyFinance.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFinance.Models
+{
+    public class PlanoContaModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Informe a descrição do plano de conta!")]
+        public string Descricao { get; set; }
+        public string Tipo { get; set; }
+        public int UsuarioId { get; set; }
+
+        public IHttpContextAccessor HttpContextAccessor { get; set; }
+
+        public PlanoContaModel()
+        {
+
+        }
+
+        //Recebe o contexto para acesso às variáveis de sessão.
+        public PlanoContaModel(IHttpContextAccessor httpContextAccessor)
+        {
+            HttpContextAccessor = httpContextAccessor;
+        }
+
+        public List<PlanoContaModel> ListaPlanoContas()
+        {
+            List<PlanoContaModel> lista = new List<PlanoContaModel>();
+
+            string query = "select Id, Descricao, Tipo, Usuario_Id from PlanoContas where Usuario_Id = " + HttpContextAccessor.HttpContext.Session.GetString("IdUsuarioLogado");
+            DAL objDAL = new DAL();
+            DataTable dt = objDAL.Reader(query);
+            DataRow[] rows = dt.Select();
+            foreach (DataRow row in rows)
+            {
+                PlanoContaModel planoConta = new PlanoContaModel();
+                planoConta.Id = int.Parse(row["Id"].ToString());
+                planoConta.Descricao = row["Descricao"].ToString();
+                planoConta.Tipo = row["Tipo"].ToString();
+                planoConta.UsuarioId = int.Parse(row["Usuario_Id"].ToString());
+                lista.Add(planoConta);
+            }
+            return lista;
+        }
+
+        public void RegistrarPlanoConta()
+        {
+            int id = int.Parse(HttpContextAccessor.HttpContext.Session.GetString("IdUsuarioLogado"));
+            string query = $"insert into PlanoContas(Descricao,Tipo,Usuario_Id)" +
+                $"values('{Descricao}','{Tipo}',{id})";
+            DAL objDAL = new DAL();
+            objDAL.NoQuery(query);
+        }
+
+        public void ExcluirPlanoConta(int id)
+        {
+            string query = "delete from PlanoContas where Id =" + id;
+            DAL objDAL = new DAL();
+            objDAL.NoQuery(query);
+        }
+
+        public PlanoContaModel CarregarRegistro(int? id)
+        {
+            string query = "select Id, Descricao, Tipo, Usuario_Id from PlanoContas where Usuario_Id = " + HttpContextAccessor.HttpContext.Session.GetString("IdUsuarioLogado")
+                + " and Id = " + id;
+            DAL objDAL = new DAL();
+            DataTable dt = objDAL.Reader(query);
+            PlanoContaModel planoConta = new PlanoContaModel();
+            planoConta.Id = int.Parse(dt.Rows[0]["Id"].ToString());
+            planoConta.Descricao = dt.Rows[0]["Descricao"].ToString();
+            planoConta.Tipo = dt.Rows[0]["Tipo"].ToString();
+            planoConta.UsuarioId = int.Parse(dt.Rows[0]["Usuario_Id"].ToString());
+
+            return planoConta;
+        }
+
+        public void AlterarPlanoConta()
+        {
+            int id = int.Parse(HttpContextAccessor.HttpContext.Session.GetString("IdUsuarioLogado"));
+            string query = $"update PlanoContas set Descricao = '{Descricao}',Tipo = '{Tipo}' where Usuario_Id = {id} and Id = {Id}";
+            DAL objDAL = new DAL();
+            objDAL.NoQuery(query);
+        }
+    }
+}
